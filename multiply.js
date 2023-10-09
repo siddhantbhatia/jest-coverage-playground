c46048a7736b1ed6f02d01cf209ad2fee6f7132f
Siddhant <sid@Siddhants-MacBook-Pro.local>
2023-10-10 00:24:10 +0800

    add all tests

---
+++ multiply.js
@@ -0,0 +1,9 @@
+function multiply(a, b) {
+    if (a < 2) {
+      return a;
+    }
+    return a * b;
+  }
+  
+  module.exports = multiply;
+  
---
+++ multiply.test.js
@@ -0,0 +1,5 @@
+const multiply = require("./multiply");
+
+test("multiply()", () => {
+  expect(multiply(5, 2)).toBe(10);
+});
